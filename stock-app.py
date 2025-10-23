de184f78c54b64276be7d901b914b4baf17a4e03
afwx2888 <irbergstrom@gmail.com>
2025-10-23 13:48:42 -0400

    Update stock-app.py

--- stock-app.py
+++ stock-app.py
@@ -7,7 +7,7 @@ import pandas as pd
 import yfinance as yf
 from datetime import datetime, timedelta
 import pytz
-import ta
+import pandas_ta as ta
 
 ##########################################################################################
 ## PART 1: Define Functions for Pulling, Processing, and Creating Technical Indicators ##
